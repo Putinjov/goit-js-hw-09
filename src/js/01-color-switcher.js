33de83e3a3b5d8a452c97c8911f853aa00c9e9ac
Putinjov <putinjov@gmail.com>
2023-03-22 20:35:26 +0200

    2 hw finished

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+function getRandomHexColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
+      }
+
+      const startBtn = document.querySelector('button[data-start]');
+      const stopBtn = document.querySelector('button[data-stop]');
+
+      let intervalId;
+
+      startBtn.addEventListener('click', () => {
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+        intervalId = setInterval(() => {
+          document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+      });
+
+      stopBtn.addEventListener('click', () => {
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+        clearInterval(intervalId);
+      });
